2398a7591e87e8530e9a9735ff2e445395f45f38
Brian Estrada <bestr2015@gmail.com>
2024-10-14 23:41:43 -0700

    Finished Button Components styled win ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { ThemeService } from './shared/services/theme.service';
+import { ButtonComponent } from './shared/components/button/button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   private themeService = inject(ThemeService);
-  toggleTheme() {
-    this.themeService.toggleTheme();
-  }
-  title = 'kanban-board';
 }
--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, inject, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './button.component.html',
-  styleUrl: './button.component.css'
+  styleUrl: './button.component.css',
 })
 export class ButtonComponent {
+  @Input() buttonType!:
+    | 'primary-l'
+    | 'primary-sm'
+    | 'secondary'
+    | 'destructive';
 
+  @Input() text!: string;
 }
