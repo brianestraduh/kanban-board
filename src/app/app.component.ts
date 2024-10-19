cdf7d7cde82535feab4cf7117ef9bf7446dca2cb
Brian Estrada <bestr2015@gmail.com>
2024-10-18 19:44:34 -0700

    Input Component initial creation and styling is done.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { ThemeService } from './shared/services/theme.service';
 import { ButtonComponent } from './shared/components/button/button.component';
 import { CheckboxComponent } from './shared/components/checkbox/checkbox.component';
+import { InputComponent } from './shared/components/input/input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ButtonComponent, CheckboxComponent],
+  imports: [ButtonComponent, CheckboxComponent, InputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
