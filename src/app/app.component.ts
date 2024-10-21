73855bcd5e789e3da1e40cc16b0e39b56b7710cd
Brian Estrada <bestr2015@gmail.com>
2024-10-21 15:00:35 -0700

    Finished Dropdown component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,21 @@ import { ThemeService } from './shared/services/theme.service';
 import { ButtonComponent } from './shared/components/button/button.component';
 import { CheckboxComponent } from './shared/components/checkbox/checkbox.component';
 import { InputComponent } from './shared/components/input/input.component';
+import { DropdownComponent } from './shared/components/dropdown/dropdown.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ButtonComponent, CheckboxComponent, InputComponent],
+  imports: [
+    ButtonComponent,
+    CheckboxComponent,
+    InputComponent,
+    DropdownComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   private themeService = inject(ThemeService);
+  options: string[] = ['To Do', 'Doing', 'Done'];
 }
